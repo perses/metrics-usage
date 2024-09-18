4987f02bc738f2115cf76093b67d17997cdb37ef
Augustin Husson <husson.augustin@gmail.com>
2024-09-18 19:00:07 +0200

    add a mutex for internal usage in the database
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- database/database.go
+++ database/database.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"sync"
+
 	v1 "github.com/perses/metrics-usage/pkg/api/v1"
 )
 
@@ -27,15 +29,26 @@ type db struct {
 	// queue is the way to send data to write in the database.
 	// There will be no other way to write in it.
 	// Doing that allows us to accept more HTTP requests to write data and to delay the actual writing.
-	// Also, we shouldn't need to lock the access to the database as there will be only one writer and multiple readers.
 	queue chan map[string]*v1.Metric
+	// We are expecting to spend more time to write data than actually read.
+	// Which result having too many writers,
+	// and so unable to read the data because the lock queue is too long to be able to access to the data.
+	// If this scenario happens,
+	// 1. Then let's flush the data into a file periodically (or once the queue is empty (if it happens))
+	// 2. Read the file directly when a read query is coming
+	// Like that we have two different ways to read and write the data.
+	mutex sync.Mutex
 }
 
 func (d *db) GetMetric(name string) *v1.Metric {
+	d.mutex.Lock()
+	defer d.mutex.Unlock()
 	return d.metrics[name]
 }
 
 func (d *db) ListMetrics() map[string]*v1.Metric {
+	d.mutex.Lock()
+	defer d.mutex.Unlock()
 	return d.metrics
 }
 
@@ -45,6 +58,7 @@ func (d *db) Enqueue(metrics map[string]*v1.Metric) {
 
 func (d *db) watchQueue() {
 	for metrics := range d.queue {
+		d.mutex.Lock()
 		for metricName, metric := range metrics {
 			if _, ok := d.metrics[metricName]; !ok {
 				d.metrics[metricName] = metric
@@ -52,5 +66,6 @@ func (d *db) watchQueue() {
 				// TODO need to merge the data
 			}
 		}
+		d.mutex.Unlock()
 	}
 }
