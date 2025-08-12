perses/metrics-usage

commit f854f8b6e563c0dcadc50746974bd9e78fd9300d

feat: add concurrency option for the labels collector (#96)
This commit adds a new field `concurrency` to the labels collector.
Because the collector needs to issue 1 HTTP request per discovered
metric name, running multiple requests in parallel reduces the overall
collection time. The default value is 1, making this an opt-in feature.

Signed-off-by: Simon Pasquier <spasquie@redhat.com>

source/labels/labels.go
@@ -15,6 +15,7 @@ package labels
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	"github.com/perses/common/async"
@@ -44,6 +45,7 @@ func NewCollector(db database.Database, cfg *config.LabelsCollector) (async.Simp
 		db:                db,
 		metricUsageClient: metricUsageClient,
 		period:            cfg.Period,
+		concurrency:       cfg.Concurrency,
 		logger:            logrus.StandardLogger().WithField("collector", "labels"),
 	}, nil
 }
@@ -54,6 +56,7 @@ type labelCollector struct {
 	db                database.Database
 	metricUsageClient client.Client
 	period            model.Duration
+	concurrency       int
 	logger            *logrus.Entry
 }
 
@@ -65,26 +68,40 @@ func (c *labelCollector) Execute(ctx context.Context, _ context.CancelFunc) erro
 		c.logger.WithError(err).Error("failed to query metrics")
 		return nil
 	}
-	result := make(map[string][]string)
+
+	var (
+		mtx    sync.Mutex
+		ch     = make(chan struct{}, c.concurrency)
+		result = make(map[string][]string, len(labelValues))
+	)
 	for _, metricName := range labelValues {
-		c.logger.Debugf("querying Prometheus to get label names for metric %s", metricName)
-		labels, _, queryErr := c.promClient.LabelNames(ctx, []string{string(metricName)}, start, now)
-		if queryErr != nil {
-			c.logger.WithError(queryErr).Errorf("failed to query labels for the metric %q", metricName)
-			continue
-		}
-		result[string(metricName)] = removeLabelName(labels)
-	}
-	if len(result) > 0 {
-		if c.metricUsageClient != nil {
-			// In this case, that means we have to send the data to a remote server.
-			if sendErr := c.metricUsageClient.Labels(result); sendErr != nil {
-				c.logger.WithError(sendErr).Error("Failed to send labels name")
+		ch <- struct{}{}
+		go func() {
+			labels := c.getLabelsForMetric(ctx, string(metricName), start, now)
+			<-ch
+			if len(labels) == 0 {
+				return
 			}
-		} else {
-			c.db.EnqueueLabels(result)
-		}
+
+			mtx.Lock()
+			result[string(metricName)] = labels
+			mtx.Unlock()
+		}()
 	}
+
+	if len(result) == 0 {
+		return nil
+	}
+
+	if c.metricUsageClient != nil {
+		// In this case, that means we have to send the data to a remote server.
+		if sendErr := c.metricUsageClient.Labels(result); sendErr != nil {
+			c.logger.WithError(sendErr).Error("Failed to send labels name")
+		}
+		return nil
+	}
+
+	c.db.EnqueueLabels(result)
 	return nil
 }
 
@@ -99,4 +116,15 @@ func removeLabelName(labels []string) []string {
 		}
 	}
 	return labels
+}
+
+func (c *labelCollector) getLabelsForMetric(ctx context.Context, metricName string, start, end time.Time) []string {
+	c.logger.Debugf("querying Prometheus to get label names for metric %s", metricName)
+	labels, _, queryErr := c.promClient.LabelNames(ctx, []string{string(metricName)}, start, end)
+	if queryErr != nil {
+		c.logger.WithError(queryErr).Errorf("failed to query labels for the metric %q", metricName)
+		return nil
+	}
+
+	return removeLabelName(labels)
 }

config/collector.go
@@ -139,8 +139,9 @@ func (c *RulesCollector) Verify() error {
 }
 
 type LabelsCollector struct {
-	Enable bool           `yaml:"enable"`
-	Period model.Duration `yaml:"period,omitempty"`
+	Enable      bool           `yaml:"enable"`
+	Period      model.Duration `yaml:"period,omitempty"`
+	Concurrency int            `yaml:"concurrency,omitempty"`
 	// MetricUsageClient is a client to send the metrics usage to a remote metrics_usage server.
 	MetricUsageClient *HTTPClient `yaml:"metric_usage_client,omitempty"`
 	HTTPClient        HTTPClient  `yaml:"prometheus_client"`
@@ -152,6 +153,9 @@ func (c *LabelsCollector) Verify() error {
 	}
 	if c.Period <= 0 {
 		c.Period = model.Duration(defaultMetricCollectorPeriodDuration)
+	}
+	if c.Concurrency <= 0 {
+		c.Concurrency = 1
 	}
 	if c.HTTPClient.URL == nil {
 		return fmt.Errorf("missing Prometheus URL for the labels collector")

commit a28b91b3f89de8d16dd74dba88273212e1ad3b71

Fix regression introduced in #96
f854f8b6 missed a proper WaitGroup to wait for completion of all Go
routines. It led to concurrent access to the map and random crashes.

Signed-off-by: Simon Pasquier <spasquie@redhat.com>

source/labels/labels.go
@@ -71,12 +71,15 @@ func (c *labelCollector) Execute(ctx context.Context, _ context.CancelFunc) erro
 
 	var (
 		mtx    sync.Mutex
+		wg     sync.WaitGroup
 		ch     = make(chan struct{}, c.concurrency)
 		result = make(map[string][]string, len(labelValues))
 	)
+	wg.Add(len(labelValues))
 	for _, metricName := range labelValues {
 		ch <- struct{}{}
 		go func() {
+			defer wg.Done()
 			labels := c.getLabelsForMetric(ctx, string(metricName), start, now)
 			<-ch
 			if len(labels) == 0 {
@@ -88,6 +91,7 @@ func (c *labelCollector) Execute(ctx context.Context, _ context.CancelFunc) erro
 			mtx.Unlock()
 		}()
 	}
+	wg.Wait()
 
 	if len(result) == 0 {
 		return nil