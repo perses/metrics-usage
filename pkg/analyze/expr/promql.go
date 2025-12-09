0ae6153a5d821a0f3e6a9b60c4481f4e2a588315
Augustin Husson <husson.augustin@gmail.com>
2025-12-09 17:41:01 +0100

    Use model.MetricNameLabel instead of __name_ everywhere (#141)
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- pkg/analyze/expr/promql.go
+++ pkg/analyze/expr/promql.go
@@ -17,6 +17,7 @@ import (
 	"regexp"
 
 	modelAPIV1 "github.com/perses/metrics-usage/pkg/api/v1"
+	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/promql/parser"
 )
 
@@ -42,7 +43,7 @@ func (a *promqlAnalyzer) Analyze(query string) (modelAPIV1.Set[string], modelAPI
 				return nil
 			}
 			for _, m := range n.LabelMatchers {
-				if m.Name == "__name__" {
+				if m.Name == model.MetricNameLabel {
 					if isValidMetricName(m.Value) {
 						metricNames.Add(m.Value)
 					} else {
